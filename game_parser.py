50e535e18c2c0a6890f243482db6ca7e7c94eb0c
doawelul <dfang314@gmail.com>
2024-09-19 10:55:43 -0700

    Add some basic image reading and cropping

--- game_parser.py
+++ game_parser.py
@@ -1,7 +1,13 @@
 import letter
+from PIL import Image as im
 
+# returns grid, turn, round and updates players gems and pts
 def get_game_state(players):
     # TODO
+    img = im.open("input.png")
+    img = img.resize((550, 500)) # from hand testing we want around 550, 500
+    grid_img = img.crop((20, 90, 335, 405)) # hand set
+    players_img = img.crop((370, 0, 540, 499)) # TODO: height needs adjustment for 4 players
     for player in players:
         player.gems = 4
         player.pts = 10
