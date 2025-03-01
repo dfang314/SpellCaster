2e7da95a890de1d28666bb568cea4a64daafb833
doawelul <dfang314@gmail.com>
2025-02-28 19:46:09 -0500

    Finish writing train cnn

--- imgs/gem_cnn.py
+++ imgs/gem_cnn.py
@@ -32,12 +32,12 @@ def train_model():
         layers.Conv2D(filters=32, kernel_size=3, padding="same", strides=1),
         layers.BatchNormalization(),
         layers.ReLU(),
-        layers.MaxPooling2D(pool_size=3, strides=2), # output is 17x17
+        layers.MaxPooling2D(pool_size=3, strides=2)
 
         layers.Conv2D(filters=16, kernel_size=3, padding="same", strides=1),
         layers.BatchNormalization(),
         layers.ReLU(),
-        layers.MaxPooling2D(pool_size=3, strides=2), # 8x8
+        layers.MaxPooling2D(pool_size=3, strides=2),
 
         layers.Flatten(),
 
@@ -50,5 +50,21 @@ def train_model():
         layers.Dense(units=1, activation="softmax"),
         
     ])
+
+    model.compile(
+        optimizer='adam',
+        loss=keras.losses.SparseCategoricalCrossentropy(),
+        metrics=['Accuracy'],
+    )
+
+    history = model.fit(
+        x=imgs,
+        y=labels,
+        epochs=100,
+    ).history
+
+    model.save("model.keras")
+
+    return model
 
 # train_model()
