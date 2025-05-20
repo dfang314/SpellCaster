df83ce50f689502b53ca9978913ddbbc1a7863fa
doawelul <dfang314@gmail.com>
2025-05-20 14:42:58 -0700

    Finish moving dl tl and double out of letter and into game engine

--- game_parser.py
+++ game_parser.py
@@ -6,9 +6,10 @@ import imgs.generate_subimg as gen
 def parse_letter(img):
     # based on letter image, figures out whether dl or tl is applicable,
     # the letter, whether there is double points, and whether there is a gem
-    return False, False, False, False, [0.01] * 26 # TODO
+    return False, False, False, False, "a" # TODO
 
-# returns grid, turn, round and updates players gems and pts
+# returns grid, turn, round, double location, dl location, tl location
+# updates players gems and pts
 def get_game_state(players):
     # most of the values here are handset
     # TODO
@@ -18,6 +19,9 @@ def get_game_state(players):
     players_img = img.crop((370, 0, 540, 499)) # TODO: height needs adjustment for 4 players
     # grid_img.show()
     grid = []
+    dl_loc = (-1, -1)
+    tl_loc = (-1, -1)
+    double_loc = (-1, -1)
     for i in range(5):
         grid.append([])
         for j in range(5):
@@ -28,17 +32,21 @@ def get_game_state(players):
             crop_bot = crop_top + 74
             letter_img = grid_img.crop((crop_left, crop_top, crop_right, crop_bot))
             # letter_img.save(f"letter_imgs/{i}_{j}.png")
-            dl, tl, double, gem, letter_probs = parse_letter(letter_img)
-            best_letter = chr(ord("a") + np.argmax(letter_probs))
-            grid[i].append(letter.Letter(best_letter))
-            grid[i][j].dl = dl
-            grid[i][j].tl = tl
-            grid[i][j].double = double
+            dl, tl, double, gem, ch = parse_letter(letter_img)
+            grid[i].append(letter.Letter(ch))
             grid[i][j].gem = gem
+
+            if dl:
+                dl_loc = (i, j)
+            if tl:
+                tl_loc = (i, j)
+            if double:
+                double_loc = (i, j)
+                
 
     for player in players:
         player.gems = 4
         player.pts = 10
     
-    return grid, 2, 3
+    return grid, 2, 3, double_loc, dl_loc, tl_loc
     
